20516eb7a746fd049c41b8784e4bb4739128e98f
manali1410 <manalijain1410@gmail.com>
2018-03-05 23:07:03 -0500

    first commit

---
+++ src/com/trv/manali/ArrayTest.java
@@ -0,0 +1,18 @@
+package com.trv.manali;
+
+public class ArrayTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] myintarray = {19,45,76,98,667};
+		int index = 0;
+		while (index<=3)
+		{
+			
+			
+		System.out.println(myintarray[index]);
+
+		index ++;
+	}
+
+	}}
